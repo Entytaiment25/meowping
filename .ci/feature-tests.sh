ac44b4aafb321122afc3333e482f44e00163f597
xor-bits <oneleh@utu.fi>
2025-09-26 00:07:52 +0300

    fix: feature-tests silently ignores failed tests

--- .ci/feature-tests.sh
+++ .ci/feature-tests.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
+TARGET_DIR="${CARGO_HOME:-.}"
+
 # Simple build check
-if [ ! -d "./target/debug" ]; then
+if [ ! -d "$TARGET_DIR/target/debug" ]; then
     echo "Build failed or target/debug directory does not exist."
     exit 1
 fi
 
-BINARY="./target/debug/meowping"
+BINARY="$TARGET_DIR/target/debug/meowping"
 
 # Check if the binary exists and is executable
 if [ ! -x "$BINARY" ]; then
@@ -16,8 +18,7 @@ if [ ! -x "$BINARY" ]; then
 fi
 
 OUTPUT1=$($BINARY 1.1.1.1 -c 1 -m -p 53)
-echo "$OUTPUT1" | grep -q "Cloudflare"
-if [ $? -ne 0 ]; then
+if ! echo "$OUTPUT1" | grep -q "Cloudflare"; then
     echo "Test failed: Expected output to contain 'Cloudflare'"
     echo "Actual output:"
     echo "$OUTPUT1"
@@ -25,11 +26,18 @@ if [ $? -ne 0 ]; then
 fi
 
 OUTPUT2=$($BINARY https://cloudflare.com -c 1 -m -p 443)
-echo "$OUTPUT2" | grep -q "AS13335 Cloudflare, Inc"
-if [ $? -ne 0 ]; then
+if ! echo "$OUTPUT2" | grep -q "AS13335 Cloudflare, Inc"; then
     echo "Test failed: Expected output to contain 'AS13335 Cloudflare, Inc' for https://cloudflare.com"
     echo "Actual output:"
     echo "$OUTPUT2"
+    exit 1
+fi
+
+OUTPUT3=$($BINARY https://cloudflare.com -c 1 -m)
+if ! echo "$OUTPUT3" | grep -q "Reply from"; then
+    echo "Test failed: Expected output to contain 'Reply from' for https://cloudflare.com"
+    echo "Actual output:"
+    echo "$OUTPUT3"
     exit 1
 fi
 
